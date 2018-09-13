1b6b172f385b8fcbdc7a3bd81ad25769bc432475
Sorbian <michal.sorbian@atos.net>
2018-09-13 11:14:40 +0200

    Completed section 3

---
+++ section_3/app.js
@@ -0,0 +1,20 @@
+// Put this in the script section in JSFiddle
+// In a local setup, you need to merge this and the index.html file into one file
+
+// new Vue({
+// 	el: '#app',
+// 	data: {
+// 		show: true
+// 	}
+// })
+
+new Vue({
+	el: '#app2',
+	data: {
+		ingredients: ['meat', 'fruit', 'cookies'],
+		persons: [
+			{name: 'Jaśko', age: 30},
+			{name: 'Kazmirz', age: 60}
+		]
+	}
+})
