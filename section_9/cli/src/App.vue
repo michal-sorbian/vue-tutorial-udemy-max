0ae45705d34e4f3cccf805e14a96bf7c861fc6db
Sorbian <michal.sorbian@atos.net>
2018-09-18 16:15:37 +0200

    Completed section 9

---
+++ section_9/cli/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+               <!-- <appQuote>
+                   <h2 slot="title">{{ quoteTitle }}</h2>
+                   <p>Description</p>
+               </appQuote> -->
+               <keep-alive>
+                <component :is="selectedComponent">
+                    <p>Default component</p>
+                </component>
+               </keep-alive>
+               
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
+
+    export default {
+        data: function () {
+            return {
+                quoteTitle: "eine quote!",
+                selectedComponent: 'appQuote'
+            }
+        },
+        components: {
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ section_9/exercise_8/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="selectedOne='appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedOne='appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedOne='appRed'">Load Red Template</button>
+                <hr>
+                <component :is="selectedOne">
+                    <p slot="blue">I'm blue</p>
+                    <p slot="green">I'm green</p>
+                    <p slot="red">I'm red</p>
+                </component>
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        },
+        data: function () {
+            return {
+                selectedOne: 'appBlue'
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
