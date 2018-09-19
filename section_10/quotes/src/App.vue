4f5accee6b5f60ec50711cefdd572e5825314735
Sorbian <michal.sorbian@atos.net>
2018-09-19 17:09:17 +0200

    Completed section 10

---
+++ section_10/quotes/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <app-header :maxQuotes="maxQuotes" :quoteCount="quotes.length"></app-header>
+        <app-new-quote @quoteAdded="addQuote($event)"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote(index)"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Click the quote to delete it.
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
+    export default {
+        data() {
+            return {
+                maxQuotes: 10,
+                quotes: [
+                    "Initial quote"
+                ]
+            }
+        },
+        components: {
+            'app-quote-grid': QuoteGrid,
+            'app-new-quote': NewQuote,
+            'app-header': Header
+        },
+        methods: {
+            addQuote(quote){
+                if(this.quotes.length < this.maxQuotes) {
+                    this.quotes.push(quote);
+                } else {
+                    alert('Reached max number of quotes');
+                }
+                
+                //console.log(this.quotes);
+            },
+            deleteQuote(index){
+                this.quotes.splice(index,1);
+                //console.log(this.quotes);
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ section_10/quotes/src/components/NewQuote.vue
@@ -0,0 +1,33 @@
+<template>
+   <div class="row">
+      <form>
+         <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+            <label>Quote</label>
+            <textarea name="" id="" rows="3" v-model="quote" class="form-control"></textarea>
+         </div>
+         <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+            <button @click.prevent="addQuote" class="btn btn-primary">Add quote</button>
+         </div>
+      </form>
+   </div>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         quote: ''
+      }
+   },
+   methods: {
+      addQuote(){
+         this.$emit('quoteAdded', this.quote);
+         this.quote = '';
+      }
+   }
+}
+</script>
+
+<style>
+   
+</style>
